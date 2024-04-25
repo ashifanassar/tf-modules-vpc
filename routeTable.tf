e76fb16b0427651bc62298dfaf28d727a0335287
ashifanassar <ashifanassar@gmail.com>
2024-04-25 23:51:03 +0530

    terraform modules

--- routeTable.tf
+++ routeTable.tf
@@ -6,6 +6,11 @@ resource "aws_route_table" "public_rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  }
+
   tags = {
     Name = "roboshop-${var.ENV}-public-rt"
   }
@@ -19,6 +24,17 @@ resource "aws_route_table" "private_rt" {
     gateway_id = aws_nat_gateway.ngw.id
   }
 
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+  }
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
   }
