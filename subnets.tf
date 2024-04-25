9510eb085a9a03091f75b31e8968296739aade85
ashifanassar <ashifanassar@gmail.com>
2024-04-25 19:22:40 +0530

    terraform modules

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,13 @@
 resource "aws_subnet" "public_subnet" {
-    count = length(var.PUBLIC_SUBNET_CIDR)
-    vpc_id     = aws_vpc.main.id
-    cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+    count                      = length(var.PUBLIC_SUBNET_CIDR)
+    vpc_id                     = aws_vpc.main.id
+    cidr_block                 = element(var.PUBLIC_SUBNET_CIDR, count.index)
+    map_public_ip_on_launch    = true 
+      availability_zone        = element(var.AZ, count.index) 
+
 
   tags = {
-    Name = "roboshop-${var.ENV}-public-subnet-${count.index+1}"
+    Name                       = "roboshop-${var.ENV}-public-subnet-${count.index+1}"
   }
 }
 
@@ -12,11 +15,13 @@ resource "aws_subnet" "public_subnet" {
 
 
 resource "aws_subnet" "private_subnet" {
-    count = length(var.PRIVATE_SUBNET_CIDR)
-    vpc_id     = aws_vpc.main.id
-    cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+    count                   = length(var.PRIVATE_SUBNET_CIDR)
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = element(var.PRIVATE_SUBNET_CIDR, count.index)
+    availability_zone         = element(var.AZ, count.index) 
+
 
   tags = {
-    Name = "roboshop-${var.ENV}-private-subnet-${count.index+1}"
+    Name                    = "roboshop-${var.ENV}-private-subnet-${count.index+1}"
   }
 }
