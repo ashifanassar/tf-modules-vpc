56c40ef316a0b4faf1e2c4cce0508de5b0600d99
ashifanassar <ashifanassar@gmail.com>
2024-04-25 23:58:17 +0530

    terraform modules

--- routeTable.tf
+++ routeTable.tf
@@ -16,6 +16,15 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+
+# Enables Public Route Table Association
+resource "aws_route_table_association" "public_subnet_rt_association" {
+  count          = length(aws_subnet.public_subnet.*.id)
+
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -38,4 +47,20 @@ resource "aws_route_table" "private_rt" {
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
   }
+}
+
+# Enables Private Route Table Association
+resource "aws_route_table_association" "private_subnet_rt_association" {
+  count          = length(aws_subnet.private_subnet.*.id)
+
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+# Add's peering route in teh default VPC Route Table 
+resource "aws_route" "default_vpc_route" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id 
 }
