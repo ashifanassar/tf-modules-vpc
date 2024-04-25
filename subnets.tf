c624795108f014b46dd9921ba9a9da3f8f01cdab
ashifanassar <ashifanassar@gmail.com>
2024-04-25 19:35:34 +0530

    terraform modules

--- subnets.tf
+++ subnets.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "public_subnet" {
     vpc_id                     = aws_vpc.main.id
     cidr_block                 = element(var.PUBLIC_SUBNET_CIDR, count.index)
     map_public_ip_on_launch    = true 
-      availability_zone        = element(var.AZ, count.index) 
+    availability_zone        = element(var.AZ, count.index+1) 
 
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_subnet" "private_subnet" {
     count                   = length(var.PRIVATE_SUBNET_CIDR)
     vpc_id                  = aws_vpc.main.id
     cidr_block              = element(var.PRIVATE_SUBNET_CIDR, count.index)
-    availability_zone         = element(var.AZ, count.index) 
+    availability_zone       = element(var.AZ, count.index+1) 
 
 
   tags = {
