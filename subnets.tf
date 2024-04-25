aada35fdb52a6dd71bcbe31c83eb34e38ae80e60
ashifanassar <ashifanassar@gmail.com>
2024-04-25 19:12:29 +0530

    terraform modules

---
+++ subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public_subnet" {
+    count = length(var.PUBLIC_SUBNET_CIDR)
+    vpc_id     = aws_vpc.main.id
+    cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-subnet-${count.index+1}"
+  }
+}
+
+
+
+
+resource "aws_subnet" "private_subnet" {
+    count = length(var.PRIVATE_SUBNET_CIDR)
+    vpc_id     = aws_vpc.main.id
+    cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-subnet-${count.index+1}"
+  }
+}
