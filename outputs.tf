b7d66aa266296a5ec42058902853319aad141d8f
ashifanassar <ashifanassar@gmail.com>
2024-05-05 23:12:37 +0530

    modules

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,10 @@
 output "VPC_ID" {
-  value = aws_vpc.main.id
+    value = aws_vpc.main.id 
 }
-
 
 output "VPC_CIDR" {
-  value = aws_vpc.main.cidr_block
+    value = aws_vpc.main.cidr_block
 }
-
 
 output "PRIVATE_SUBNET_IDS" {
     value =  aws_subnet.private_subnet.*.id
@@ -16,11 +14,9 @@ output "PUBLIC_SUBNET_IDS" {
     value = aws_subnet.public_subnet.*.id
 }
 
-
 output "PRIVATE_SUBNET_CIDR" {
     value = aws_subnet.private_subnet.*.cidr_block
 }
-
 
 output "PUBLIC_SUBNET_CIDR" {
     value = aws_subnet.public_subnet.*.cidr_block
@@ -36,18 +32,19 @@ output "DEFAULT_VPC_CIDR" {
 
 
 
-output "PUBLIC_HOSTEDZONE_ID" {
-    value = var.PUBLIC_HOSTEDZONE_ID
-}
 
-output "PUBLIC_HOSTEDZONE_NAME" {
-    value = var.PUBLIC_HOSTEDZONE_NAME
-}
+# output "PUBLIC_HOSTEDZONE_ID" {
+#     value = var.PUBLIC_HOSTEDZONE_ID
+# }
 
-output "PRIVATE_HOSTEDZONE_ID" {
-    value = var.PRIVATE_HOSTEDZONE_ID
-}
+# output "PUBLIC_HOSTEDZONE_NAME" {
+#     value = var.PUBLIC_HOSTEDZONE_NAME
+# }
 
-output "PRIVATE_HOSTEDZONE_NAME" {
-    value = var.PRIVATE_HOSTEDZONE_NAME
-}
+# output "PRIVATE_HOSTEDZONE_ID" {
+#     value = var.PRIVATE_HOSTEDZONE_ID
+# }
+
+# output "PRIVATE_HOSTEDZONE_NAME" {
+#     value = var.PRIVATE_HOSTEDZONE_NAME
+# }
