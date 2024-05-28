08dee45f7aae84cd40793b3deb2b2f3c5c73c8b1
ashifanassar <ashifanassar@gmail.com>
2024-05-28 19:58:49 +0530

    modules

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
-resource "aws_vpc_peering_connection" "peering" {
+# resource "aws_vpc_peering_connection" "peering" {
 
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.DEFAULT_VPC_ID
-  auto_accept   = true
-}
+#   peer_vpc_id   = aws_vpc.main.id
+#   vpc_id        = var.DEFAULT_VPC_ID
+#   auto_accept   = true
+# }
